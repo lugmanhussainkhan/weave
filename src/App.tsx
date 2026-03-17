c5ce57e2b17db4b499d21396bc8795472e6f4ebc
LugmanS <lugmanhussainkhan@gmail.com>
2026-03-17 14:52:29 +0530

    chore:added nav and input

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,61 @@
+import { ArrowUp } from "lucide-react";
+import { useState } from "react";
 import { Button } from "./components/ui/button";
+import { Textarea } from "./components/ui/textarea";
 
 export default function App() {
+  const [input, setInput] = useState("");
+
+  function sendMessage() {
+    console.log(input);
+    setInput("");
+  }
+
   return (
-    <main>
-      <nav className="h-14 border-b">
+    <main className="min-h-screen bg-card">
+      <nav className="h-16 w-full fixed top-0 left-0 bg-card/60 backdrop-blur-2xl z-50">
         <div className="flex items-center justify-between px-4 h-full max-w-3xl mx-auto">
-          <h1 className="font-mono">map_out</h1>
+          <div className="flex items-center gap-2">
+            <img src="/logo.svg" className="size-8 hover:animate-spin" />
+            <div>
+              <h1 className="font-mono">map_out</h1>
+              <p className="text-xs text-muted-foreground">by Madhi AI</p>
+            </div>
+          </div>
           <Button variant={"secondary"} size={"sm"}>
             New chat
           </Button>
         </div>
       </nav>
+      <div className="max-w-3xl w-full mx-auto fixed bottom-0 bg-card left-1/2 transform translate-x-[-50%] z-50 pb-3.5">
+        <div className="p-2 border rounded-xl bg-muted">
+          <Textarea
+            value={input}
+            onChange={(e) => setInput(e.target.value)}
+            onKeyDown={(e) => {
+              if (e.key === "Enter") {
+                if (e.shiftKey) {
+                  return;
+                }
+                e.preventDefault();
+                sendMessage();
+              }
+            }}
+            autoFocus
+            className="border-0 bg-transparent dark:bg-transparent shadow-none focus-visible:ring-0 resize-none"
+            placeholder="How can I help you?"
+          />
+          <div className="flex items-center justify-end">
+            <Button
+              size={"icon"}
+              disabled={input.length === 0}
+              onClick={() => sendMessage()}
+            >
+              <ArrowUp />
+            </Button>
+          </div>
+        </div>
+      </div>
     </main>
   );
 }
